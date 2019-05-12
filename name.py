0b5dd05ca26f3af3d74b9032dfed0d052dd624d9
Kingzeke22 <49129056+Kingzeke22@users.noreply.github.com>
2019-05-12 10:39:39 -0400

    Add files via upload

---
+++ name.py
@@ -0,0 +1,19 @@
+name = 'ada lovelace'
+print(name.title())
+
+first_name = 'ada'
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+
+print(first_name)
+print(last_name)
+print(full_name)
+
+print(first_name.title())
+print(last_name.title())
+print(full_name.title())
+
+print(first_name.title() + ' ' + last_name.title())
+
+message = 'Hello, ' + full_name.title() + '!'
+print(message)
